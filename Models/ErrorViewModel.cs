48527d8a5304138564b7520376fb6980e16f7f6b
LDHANIREDDY <lokanathdhanireddy@gmail.com>
2025-10-18 18:57:04 -0500

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyNewsPortals.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
