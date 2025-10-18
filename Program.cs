a5d219de5930f2683115926170f2f28d522daaaa
LDHANIREDDY <lokanathdhanireddy@gmail.com>
2025-10-18 18:56:18 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using MyNewsPortals.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+var newsApiKey = "020f9b9ec3314984b32f9b47af24d0d2";
+builder.Services.AddSingleton(new NewsService(newsApiKey));
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
